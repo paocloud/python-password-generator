37fa983a236534c249cc0d8d179d7b85c1dd2306
PayungsakCNR <payungsakpk@payungsakpk.xyz>
2021-01-23 10:49:57 +0700

    Add no punc pass.

--- password-generator.py
+++ password-generator.py
@@ -7,9 +7,17 @@ import string
 from random import *
 
 characters = string.ascii_letters + string.punctuation  + string.digits
+charactersNopunc = string.ascii_letters + string.digits
+
+def calPassNopunc():
+    return "".join(choice(charactersNopunc) for x in range(randint(8, 16)))
 
 def calPass():
     return "".join(choice(characters) for x in range(randint(8, 16)))
 
 for i in range(5):
  print (calPass())
+
+for i in range(5):
+ print (calPassNopunc())
+
